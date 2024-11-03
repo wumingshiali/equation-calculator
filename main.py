3cfe1374e6bc63396a581d6957d63a7fe96e1cf3
wumingshiali <157328007+wumingshiali@users.noreply.github.com>
2024-11-03 20:19:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
 import tkinter as tk
 from tkinter import messagebox
-import tkintertools,os
-script_dir = os.path.dirname(os.path.abspath(__file__))
-icon_path = os.path.join(script_dir, 'logojsq.ico')
+import tkintertools,os,json
+script_dir = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_dir)
 window = tkintertools.Tk(title="计算器")
 window.alpha(0.995)
 window.center()
-window.iconbitmap(icon_path)
+window.iconbitmap(os.path.join(script_dir, 'logojsq.ico'))
 canvas = tkintertools.Canvas(window, zoom_item=True, keep_ratio="min", free_anchor=True)
 canvas.place(width=1280, height=720, x=640, y=360, anchor="center")
 tkintertools.Text(canvas, (700, 200), text="计 算 器", fontsize=48, anchor="center")
-entry = tkintertools.InputBox(canvas,position=(600,250))
+tkintertools.Text(canvas, (600, 240), text="算式", anchor="nw")
+entry = tkintertools.InputBox(canvas,position=(600,270))
+try:
+    with open(script_dir+"\settingjsq.json",'r') as f:
+        i = json.load(f)
+        if i["AutoAddDY"] == "True":
+            entry.set("1+")
+except FileNotFoundError:
+    messagebox.showerror("错误","未找到配置文件，请恢复配置文件")
+except json.decoder.JSONDecodeError:
+    messagebox.showerror("错误","配置文件格式错误，请恢复配置文件")
+except:
+    messagebox.showerror("错误","未知错误，请重新安装软件或提交Issue")
 def change_state():
     var = entry.get()
     try:
         messagebox.showinfo("结果",(eval(var)))
     except:
         messagebox.showerror("错误","算式错误")
-button = tkintertools.Button(canvas,text='计算',command=change_state,position=(600,300))
+    del var
+def setting():
+    global icon_path
+    tl = tkintertools.Toplevel(window,title="计算器")
+    window.alpha(0.995)
+    window.iconbitmap(os.path.join(script_dir, 'logojsq.ico'))
+    canvas = tkintertools.Canvas(tl, zoom_item=True, keep_ratio="min", free_anchor=True)
+    canvas.place(width=1280, height=720, x=640, y=360, anchor="center")
+    tkintertools.Text(canvas, (20, 10), text="自动将输出的结果加上算式", anchor="nw")
+    tkintertools.Switch(canvas, (20, 40), command=orca,default=True)
+    tkintertools.Button(canvas, text="保存设置",position=(20, 80),command=save_setting)
+    tl.mainloop()
+def orca(a):
+    global f,i,script_dir
+    try:
+        with open(script_dir+"\settingjsq.json",'r') as f:
+            i = json.load(f)
+            if i["AutoAddDY"] == "True":
+                i["AutoAddDY"] = "False"
+            else:
+                i["AutoAddDY"] = "True"
+
+    except FileNotFoundError:
+        messagebox.showerror("错误","未找到配置文件，请恢复配置文件")
+    except json.decoder.JSONDecodeError:
+        messagebox.showerror("错误","配置文件格式错误，请恢复配置文件")
+    except:
+        messagebox.showerror("错误","未知错误，请重新安装软件")
+def save_setting():
+    global f,i
+    with open(script_dir+"\settingjsq.json",'w') as f:
+        json.dump(i,f,indent=4)
+button = tkintertools.Button(canvas,text='计算',command=change_state,position=(600,320))
+button = tkintertools.Button(canvas,text='设置',command=setting,position=(600,370))
 window.mainloop()
