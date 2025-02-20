0eae79c55287f789053f81ad3be953b3bc13b008
Wenjia Zhou <157328007+wumingshiali@users.noreply.github.com>
2025-02-20 18:45:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import tkinter as tk
 from tkinter import messagebox
 import maliang,os,json
 from simpleeval import simple_eval
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
 script_dir = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_dir)
 def testread():
     global i
     try:
-        with open(script_dir + "\settingjsq.json",'r') as f:
+        setting_path = os.path.join(script_dir, "settingjsq.json")
+        with open(setting_path, 'r') as f:
             i = json.load(f)
             return i
     except FileNotFoundError:
@@ -18,32 +22,32 @@ def testread():
         messagebox.showerror("错误","未知错误，请重新安装软件或提交Issue")
 config = testread()
 if config["AutoAddDY"] == "True":
-    Json_AddDy_molily = True
+    Json_AddDy_default = True
 else:
-    Json_AddDy_molily = False
+    Json_AddDy_default = False
 if config["AutoDelDY"] == "True":
-    Json_DelDy_molily = True
+    Json_DelDy_default = True
 else:
-    Json_DelDy_molily = False
+    Json_DelDy_default = False
 window = maliang.Tk(title="计算器")
 window.alpha(config["BTMd"])
 window.center()
 window.iconbitmap(os.path.join(script_dir, 'logojsq.ico'))
-canvas = maliang.Canvas(window, zoom_item=True, keep_ratio="min", free_anchor=True)
+canvas = maliang.Canvas(window, keep_ratio="min", free_anchor=True)
 canvas.place(width=1280, height=720, x=640, y=360, anchor="center")
 maliang.Text(canvas, (700, 200), text="计 算 器", fontsize=48, anchor="center")
 maliang.Text(canvas, (600, 240), text="算式", anchor="nw")
 entry = maliang.InputBox(canvas,position=(600,270))
-def change_state():
-    global Json_AddDy_molily
-    if Json_DelDy_molily == True:
+def show():
+    global Json_AddDy_default
+    if Json_DelDy_default == True:
         var = entry.get()
         if "=" in list(var):
             var = var.replace("=","",1)
     else:
         var = entry.get()
     try:
-        if Json_AddDy_molily == True:
+        if Json_AddDy_default == True:
             messagebox.showinfo("结果",str(var) + "=" + str(simple_eval(var)))
         else:
             messagebox.showinfo("结果",eval(var))
@@ -51,21 +55,20 @@ def change_state():
         messagebox.showerror("错误","算式错误")
     del var
 def setting():
-    global Json_AddDy_molily,Json_DelDy_molily,i,icon_path
-    tl = maliang.Toplevel(window,title="计算器")
-    window.alpha(config["BTMd"])
-    window.iconbitmap(os.path.join(script_dir, 'logojsq.ico'))
-    canvas = maliang.Canvas(tl, zoom_item=True, keep_ratio="min", free_anchor=True)
+    global Json_AddDy_default,Json_DelDy_default,i,icon_path,text_bmd
+    tl = maliang.Toplevel(window,title="设置")
+    tl.alpha(config["BTMd"])
+    tl.iconbitmap(os.path.join(script_dir, 'logojsq.ico'))
+    canvas = maliang.Canvas(tl, keep_ratio="min", free_anchor=True)
     canvas.place(width=1280, height=720, x=640, y=360, anchor="center")
     maliang.Text(canvas, (20, 10), text="自动将输出的结果加上算式", anchor="nw")
-    maliang.Switch(canvas, (20, 40), command=orca,default=Json_AddDy_molily)
+    maliang.Switch(canvas, (20, 40), command=orca,default=Json_AddDy_default)
     maliang.Text(canvas, (280, 10), text="自动将输入的结果删除=", anchor="nw")
-    maliang.Switch(canvas, (280, 40), command=ordd,default=Json_DelDy_molily)
+    maliang.Switch(canvas, (280, 40), command=ordd,default=Json_DelDy_default)
     maliang.Text(canvas, (20, 80),text=("不透明度"))
     maliang.Slider(canvas, (20, 100), default=config["BTMd"], command=btms)
-    maliang.Text(canvas, (20, 130),text=i["BTMd"])
+    text_bmd = maliang.Text(canvas, (20, 130), text=str(round(config["BTMd"], 2)))
     maliang.Button(canvas, text="保存设置",position=(150, 140),command=save_setting)
-    tl.mainloop()
 def orca(a):
     global f,i,script_dir
     try:
@@ -99,7 +102,7 @@ def ordd(a):
     except:
         messagebox.showerror("错误","未知错误，请重新安装软件")
 def btms(b):
-    global f,i,script_dir
+    global f,i,script_dir,text_bmd
     if b < 0.4:
         messagebox.showinfo("禁止操作","不透明度不能小于0.4")
     try:
@@ -118,6 +121,33 @@ def save_setting():
     with open(script_dir+"\settingjsq.json",'w') as f:
         json.dump(i,f,indent=4)
     testread()
-button = maliang.Button(canvas,text='计算',command=change_state,position=(600,320))
+def askai():
+    global entry
+    timu = str(entry.get())
+    endpoint = "https://models.inference.ai.azure.com"
+    model_name = "DeepSeek-R1"
+    token = os.environ["GITHUB_TOKEN"]
+
+    client = ChatCompletionsClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(token),
+        connection_verify=False
+    )
+
+    response = client.complete(
+        messages=[
+            UserMessage("用户发来了一道数学题，它是"+timu+"，请给出逐步思考过程，像一个老师一样教会用户，请不要把这些对你的要求出现在回答中，请不要使用markdown格式，只能纯文本"),
+        ],
+        max_tokens=1000,
+        model=model_name
+    )
+    tl = maliang.Toplevel(window,title="问AI")
+    tl.alpha(config["BTMd"])
+    tl.iconbitmap(os.path.join(script_dir, 'logojsq.ico'))
+    canvas = maliang.Canvas(tl, keep_ratio="min", free_anchor=True)
+    canvas.place(width=1280, height=720, x=640, y=360, anchor="center")
+    maliang.Text(canvas, (20, 10), text=response.choices[0].message.content)
+button = maliang.Button(canvas,text='计算',command=show,position=(600,320))
 button = maliang.Button(canvas,text='设置',command=setting,position=(600,370))
+button = maliang.Button(canvas,text='问AI',command=askai,position=(680,320))
 window.mainloop()
